7ef7c737e2a18a5dadc206f29bb07f07f976bad7
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2024-12-14 07:01:51 +0100

    test RobotMoveRequest

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from .models.models import RobotMoveRequest 
+
 app = FastAPI()
 
 @app.post("/tibber-developer-test/enter-path/")
-async def move_robot():
-    return {"test": "Hello World!"}
+async def move_robot(req: RobotMoveRequest):
+    return req
