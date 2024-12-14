d50b1b3ab4f45b1f36c38e9a35dc291dc6eb2ddb
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2024-12-14 20:33:40 +0100

    move db commit logic to own function to make testable

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from time import perf_counter
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 
-from .db import Execution, SessionDependency, create_db_and_tables
-from .models.models import RobotMoveRequest 
-from .move import clean_office
+from .db import SessionDependency, create_db_and_tables, commit_execution
+from .models.models import Execution, RobotMoveRequest 
+from .actions.move import clean_office
 
 app = FastAPI()
 
@@ -24,8 +24,6 @@ async def move_robot(req: RobotMoveRequest, session: SessionDependency):
             result=total_cleaned_tiles,
             duration=end_time-start_time,
     )
-    session.add(execution)
-    session.commit()
-    session.refresh(execution)
+    commit_execution(execution, session)
 
     return jsonable_encoder(execution)
