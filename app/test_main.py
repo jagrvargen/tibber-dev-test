528ffbf3ee47521fab6ff88dd1c0e8c7b80851e9
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2024-12-14 07:50:19 +0100

    create test file

---
+++ app/test_main.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_move_robot():
+    resp = client.post(
+            "/tibber-developer-test/enter-path/",
+            json={"start": 
+                  {
+                      "x": 0, "y": 0
+                    },
+                  "commands": [
+                      {
+                        "direction": "north",
+                        "steps": 1
+                      }
+                  ]
+            }
+    )
+    assert resp.status_code == 200
